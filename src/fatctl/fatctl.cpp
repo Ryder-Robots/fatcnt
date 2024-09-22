8a08cb7b5833d2dc46955b9806f2fae1c54d1b3c
Aaron <azzmosphere@gmail.com>
2024-09-22 07:08:38 +0000

    * initial webserver created.

--- src/fatctl/fatctl.cpp
+++ src/fatctl/fatctl.cpp
@@ -1,6 +1,22 @@
 #include "fatctl.h"
 
+using namespace rrobot;
 
 int main() {
+
+    // Create webserver
+    fatcnt_server fs = fatcnt_server(
+         httpserver::webserver(httpserver::create_webserver(8080)
+         .start_method(httpserver::http::http_utils::INTERNAL_SELECT)
+         .max_threads(5))
+    );
+
+    // create resource
+    rr_connection connection = rr_connection();
+    fs.register_resource(&connection, "/connection");
+
+    // start server
+    fs.create();
+
     return 0;
 }
