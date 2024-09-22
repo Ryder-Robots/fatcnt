701b17d8f7bb3dd67c419783971d994ed00c6123
Aaron <azzmosphere@gmail.com>
2024-09-22 07:15:06 +0000

    * Added change to expose 8081 port for testing the client sorfware

--- src/fatctl/fatctl.cpp
+++ src/fatctl/fatctl.cpp
@@ -4,9 +4,10 @@ using namespace rrobot;
 
 int main() {
 
+    //TODO: this should all come from an uploaded config
     // Create webserver
     fatcnt_server fs = fatcnt_server(
-         httpserver::webserver(httpserver::create_webserver(8080)
+         httpserver::webserver(httpserver::create_webserver(8081)
          .start_method(httpserver::http::http_utils::INTERNAL_SELECT)
          .max_threads(5))
     );
