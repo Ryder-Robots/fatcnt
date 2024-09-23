89cd3d263942d9607e4a6a333f8ca8717e2882b4
Aaron <azzmosphere@gmail.com>
2024-09-23 09:01:13 +0000

    * Added websocket support

--- src/fatctl/fatctl.cpp
+++ src/fatctl/fatctl.cpp
@@ -3,21 +3,17 @@
 using namespace rrobot;
 
 int main() {
+    // Create state first
+    rrobot::rr_state_c *RR_STATE = new rrobot::rr_state_c();
 
     //TODO: this should all come from an uploaded config
     // Create webserver
-    fatcnt_server fs = fatcnt_server(
-         httpserver::webserver(httpserver::create_webserver(8081)
-         .start_method(httpserver::http::http_utils::INTERNAL_SELECT)
-         .max_threads(5))
-    );
-
-    // create resource
-    rr_connection connection = rr_connection();
-    fs.register_resource(&connection, "/connection");
+    fatcnt_server fs = fatcnt_server();
 
     // start server
     fs.create();
+
+    while(1) {}
 
     return 0;
 }
