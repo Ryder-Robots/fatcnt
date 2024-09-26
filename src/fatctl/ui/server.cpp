1f52b83e212a1434d056ccfc0e81ec1e1e077ae1
Aaron <azzmosphere@gmail.com>
2024-09-26 10:29:40 +0000

    * Added handlers for and upgrade path.

--- src/fatctl/ui/server.cpp
+++ src/fatctl/ui/server.cpp
@@ -10,18 +10,58 @@
 
 #include "server.hpp"
 
+#include <iostream>
+
+using namespace std;
 using namespace rrobot;
+using json = nlohmann::json;
+
+json PAGE_INVALID_WEBSOCKET_REQUEST = {
+    {"success", "false"},
+    {"payload", ""},
+    {"error", {{"code", MHD_HTTP_BAD_REQUEST}, {"message", "Invalid websocket request"}}}};
+
+bool fatcnt_server::is_valid(struct MHD_Connection *connection, const char *version) {
+    bool is_valid = true;
+    const char *value = NULL;
+    char sec_websocket_accept[29];
+
+    /* check whether an websocket upgrade is requested */
+    if (MHD_WEBSOCKET_STATUS_OK != MHD_websocket_check_http_version(version)) {
+        is_valid = false;
+    }
+    value = MHD_lookup_connection_value(connection, MHD_HEADER_KIND, MHD_HTTP_HEADER_CONNECTION);
+    if (MHD_WEBSOCKET_STATUS_OK != MHD_websocket_check_connection_header(value)) {
+        is_valid = false;
+    }
+    value = MHD_lookup_connection_value(connection, MHD_HEADER_KIND, MHD_HTTP_HEADER_UPGRADE);
+    if (MHD_WEBSOCKET_STATUS_OK != MHD_websocket_check_upgrade_header(value)) {
+        is_valid = false;
+    }
+    value = MHD_lookup_connection_value(connection, MHD_HEADER_KIND, MHD_HTTP_HEADER_SEC_WEBSOCKET_VERSION);
+    if (MHD_WEBSOCKET_STATUS_OK != MHD_websocket_check_version_header(value)) {
+        is_valid = false;
+    }
+    value = MHD_lookup_connection_value(connection, MHD_HEADER_KIND, MHD_HTTP_HEADER_SEC_WEBSOCKET_KEY);
+    if (MHD_WEBSOCKET_STATUS_OK != MHD_websocket_create_accept_header(value, sec_websocket_accept)) {
+        is_valid = false;
+    }
+
+    return is_valid;
+}
 
 /**
  * @fn access_handler
  * @brief entry point to handle access for user input.
  * @param *cls
  */
-static enum MHD_Result access_handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method,
-                                      const char *version, const char *upload_data, size_t *upload_data_size,
-                                      void **req_cls) {
+enum MHD_Result fatcnt_server::access_handler(void *cls, struct MHD_Connection *connection, const char *url,
+                                              const char *method, const char *version, const char *upload_data,
+                                              size_t *upload_data_size, void **req_cls) {
     static int aptr;
-    *req_cls = NULL;
+    int ret = MHD_NO;
+    struct MHD_Response *response;
+    char sec_websocket_accept[29];
 
     if (0 != strcmp(method, "GET")) {
         // Unexpected method
@@ -30,16 +70,25 @@ static enum MHD_Result access_handler(void *cls, struct MHD_Connection *connecti
         /* do never respond on first call */
         *req_cls = &aptr;
         return MHD_YES;
-    } else if (0 == strcmp(url, CONNECTION_URL)) {
-        //inital connection. TODO: need the JSON for this
-        struct MHD_Response *response;
-    }
+    } else if (0 != strcmp(url, CONNECTION_URL)) {
+        // invalid response.
+        ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+        MHD_destroy_response(response);
 
-    struct MHD_Response *response;
-    response = MHD_create_response_from_buffer_static(strlen(PAGE_NOT_FOUND), PAGE_NOT_FOUND);
-    int ret = MHD_queue_response(connection, MHD_HTTP_NOT_FOUND, response);
-    MHD_destroy_response(response);
-    return (MHD_Result)ret;
+    } else if (is_valid(connection, version)) {
+        response = MHD_create_response_for_upgrade(&upgrade_handler, NULL);
+        MHD_add_response_header(response, MHD_HTTP_HEADER_UPGRADE, "websocket");
+        MHD_add_response_header(response, MHD_HTTP_HEADER_SEC_WEBSOCKET_ACCEPT, sec_websocket_accept);
+        ret = MHD_queue_response(connection, MHD_HTTP_SWITCHING_PROTOCOLS, response);
+        MHD_destroy_response(response);
+    } else {
+        /* return error page */
+        response = MHD_create_response_from_buffer_static(PAGE_INVALID_WEBSOCKET_REQUEST.dump().length(),
+                                                          PAGE_INVALID_WEBSOCKET_REQUEST.dump().c_str());
+        ret = MHD_queue_response(connection, MHD_HTTP_BAD_REQUEST, response);
+        MHD_destroy_response(response);
+    }
+    return static_cast<MHD_Result>(ret);
 }
 
 /**
@@ -65,3 +114,34 @@ void fatcnt_server::create() {
 }
 
 void fatcnt_server::close() { MHD_stop_daemon(_ws); }
+
+void fatcnt_server::upgrade_handler(void *cls, struct MHD_Connection *connection, void *req_cls, const char *extra_in,
+                                    size_t extra_in_size, MHD_socket fd, struct MHD_UpgradeResponseHandle *urh) {
+    pthread_t pt;
+
+    __gnu_cxx::stdio_filebuf<char> filebuf(fd, std::ios::in);
+    istream ifs(&filebuf);
+    try {
+        json j = json::parse(ifs);
+
+        // validate the username and password at this point.
+
+    } catch (const std::exception &e) {
+        //TODO: throw an exception that 
+        struct MHD_Response *response = MHD_create_response_from_buffer_static(PAGE_INVALID_WEBSOCKET_REQUEST.dump().length(),
+                                                          PAGE_INVALID_WEBSOCKET_REQUEST.dump().c_str());
+        MHD_queue_response(connection, MHD_HTTP_BAD_REQUEST, response);
+        MHD_destroy_response(response);
+        return;
+    }
+
+    // if (0 != pthread_create(&pt, NULL, &rr_env_communicator, cu)) abort();
+    // pthread_detach(pt);
+}
+
+/** 
+ * Control the drone from this point, the user has been authenticated, so this should be considred safe. 
+ */
+void *fatcnt_server::rr_env_communicator(void* cu) {
+    return static_cast<void*>(cu);
+}
--- src/fatctl/ui/server.hpp
+++ src/fatctl/ui/server.hpp
@@ -5,12 +5,16 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
-#include <iostream>
-#include <stdexcept>
 #include <microhttpd.h>
 #include <microhttpd_ws.h>
-#include <string.h>
 #include <stdint.h>
+#include <string.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <ext/stdio_filebuf.h>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
 #include "fatctl/environment/state.hpp"
 
@@ -20,21 +24,50 @@
 #define TIMEOUT (uint32_t)120
 
 // Create authorization header.
-#define CONNECTION_URL "/connection"
+#define CONNECTION_URL "/"
 
-namespace rrobot
-{
-    class fatcnt_server
-    {
-    public:
-        fatcnt_server() : _ws(NULL) {}
+/**
+ * @brief websocket server.
+ */
+namespace rrobot {
+class fatcnt_server {
+   public:
+    /**
+     *  @fn fatcnt_server
+     *  @brief class constructor
+     */
+    fatcnt_server() : _ws(NULL) {}
 
-        void create();
-        void close();
+    /**
+     * @fn create
+     * @brief creates an instance of the server.
+     */
+    void create();
 
-    private:
-        struct MHD_Daemon *_ws;
-    };
-}
+    /**
+     * @fn close
+     * @brief stops the server
+     */
+    void close();
+   protected:
+    static void upgrade_handler(void *cls, struct MHD_Connection *connection, void *req_cls, const char *extra_in,
+                                size_t extra_in_size, MHD_socket fd, struct MHD_UpgradeResponseHandle *urh);
+
+    static enum MHD_Result access_handler(void *cls, struct MHD_Connection *connection, const char *url,
+                                          const char *method, const char *version, const char *upload_data,
+                                          size_t *upload_data_size, void **req_cls);
+
+   private:
+    struct MHD_Daemon *_ws;
+
+    static bool is_valid(struct MHD_Connection *connection, const char *version);
+
+    /** 
+     * @fn rr_env_communicator
+     * @brief use to communicate to enivoronment, actions are recieved from the UI, and observations and transmitted back to UI.
+     */
+    static void *rr_env_communicator(void* cu);
+   };
+}  // namespace rrobot
 
 #endif
