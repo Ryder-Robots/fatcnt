c5d93b0225d3b2ecfc54084efedfe55a9375c341
Aaron <azzmosphere@gmail.com>
2024-10-04 11:15:26 +0000

    * Got program interacting with controller.

--- src/fatctl/fatctl.h
+++ src/fatctl/fatctl.h
@@ -5,7 +5,12 @@
 #ifndef FATCTL_H
 #define FATCTL_H
 
+#include <dlib/logger.h>
+#include <dlib/misc_api.h>
+
 #include "fatctl/ui/server.hpp"
 #include "fatctl/environment/state.hpp"
+
+#define DEFAULT_PORT 8081
 
 #endif
--- src/fatctl/ui/server.hpp
+++ src/fatctl/ui/server.hpp
@@ -12,6 +12,8 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include <stdexcept>
+#include <dlib/logger.h>
+#include <dlib/misc_api.h>
 
 #include "fatctl/environment/state.hpp"
 #include "fatctl/ui/websocket_status_code.hpp"
@@ -20,6 +22,10 @@
 
 // Create authorization header.
 #define CONNECTION_URL "/"
+
+// TODO: These should come from common headers.  but lets code it here for now
+#define RR_CMD_EXIT 0x0e
+
 using json = nlohmann::json;
 
 /**
@@ -51,7 +57,7 @@ class fatcnt_server {
     * @brief recieves inbound messages (in its own thread)
     * @param socket connection to environment
     */
-    static void *recieve(void *senv);
+    static json* recieve(socket_env *senv, char *buffer);
 
     /**
      * @fn send
@@ -60,9 +66,18 @@ class fatcnt_server {
      */
     static void *send(void *senv);
 
+    /** 
+     * @fn accept
+     * @brief ready to accept conenctions.
+     */
+    pthread_t rr_accept();
+
    private:
     int _sockfd;
     int _max_connections = 5;
+    socket_env *_sockenv = new socket_env();
+
+    static void *accept_conn(void *senv);
 };
 }  // namespace rrobot
 
