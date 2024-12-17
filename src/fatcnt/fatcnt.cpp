c2e46466b053dcdaa1d02571baa16aab04dd9730
Aaron Spiteri <azzmosphere@gmail.com>
2024-12-17 12:05:04 +1100

    Mapper (#17)
    * integratrated external server.
    
    * made external virtual because it's an interface, not a class.
    
    * added mapper factory
    
    * added code to integrate main program
    
    * Added main code for drone.
    
    * Added documentation for main program.

--- src/fatcnt/fatcnt.cpp
+++ src/fatcnt/fatcnt.cpp
@@ -1,5 +1,29 @@
 #include "fatcnt.hpp"
 
+/**
+ * MIT License
+ *
+ * Copyright (c) 2024 Ryder Robots
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 using namespace rrobot;
 namespace po = boost::program_options;
 
@@ -8,6 +32,12 @@ dlib::logger dlog_main("rr_robot_main");
 StateIface* volatile state = nullptr;
 thread* volatile tmain = nullptr;
 
+/**
+ * @fn signalHandler
+ * @brief
+ * handles system generated interupts.
+ * @param signum signale getting sent from system.
+ */
 void signalHandler(int signum) {
     if (signum == SIGTERM) {
         dlog_main << dlib::LINFO << "SIGTERM has been, begining shutdown process";
@@ -30,6 +60,15 @@ void signalHandler(int signum) {
     exit(EXIT_SUCCESS);
 }
 
+/**
+ * @fn main
+ * @brief
+ * entry point, launches catagorizer (main handler).  Catagorizer launches all other handlers, with configuration 
+ * provided from mapper.  Mapper will vary depending on which manifest is given as a command line object, and 
+ * provides constants (environment), including specifics on drone hardware.
+ * 
+ * Mapper will also initilize any handlers needed that are specific to the drone hardware.
+ */
 int main(int argc, char *argv[]) {
     po::options_description desc("Allowed options");
     desc.add_options()
