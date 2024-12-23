6ad9d151eb3850a8ec4bcbd1c621452c26c0c71b
Aaron Spiteri <azzmosphere@gmail.com>
2024-12-23 21:19:20 +1100

    added bitmask

--- src/fatcnt/state/statusprocessor.cpp
+++ src/fatcnt/state/statusprocessor.cpp
@@ -4,8 +4,7 @@ using namespace rrobot;
 
 RR_CMODES StatusProcessor::getMode() {
     int32_t flags = _state->getFlags();
-    flags -= (flags & INITILIZING) + (flags & ERROR) + (flags & RELOADING) + 
-        (flags + SHUTTING_DOWN) + (flags & TERMINATED) + (flags & ACTIVE);
+    flags -= MODE_BITMASK;
     return static_cast<RR_CMODES>(flags);
 }
 
@@ -17,8 +16,9 @@ RRP_STATUS StatusProcessor::getStatus() {
     int32_t flags_modes = _state->getFlags() & CMODE_MANUAL_FLIGHT + _state->getFlags() & CMODE_NOT_SET;
 
     RRP_STATUS status = RRP_STATUS::INITILIZING;
-    vector<RRP_STATUS> statusArray = {TERMINATED, SHUTTING_DOWN, ERROR, RELOADING, INITILIZING, ACTIVE};
+    vector<RRP_STATUS> statusArray = STATUS_ARRAY_INIT;
 
+    // This is the a precise order on what global status will be.
     for (auto s : statusArray) {
         if ((statuses & s) == s) {
             status = s;
@@ -32,7 +32,6 @@ RRP_STATUS StatusProcessor::getStatus() {
 
 void StatusProcessor::setMode(RR_CMODES mode) {
     int32_t flags = _state->getFlags();
-    int32_t statuses = (flags & INITILIZING) + (flags & ERROR) + (flags & RELOADING) + 
-        (flags + SHUTTING_DOWN) + (flags & TERMINATED) + (flags & ACTIVE);    
+    int32_t statuses = MODE_BITMASK;    
     _state->setFlags(0 | statuses | mode);
 }
