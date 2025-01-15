a13e3844ffc73a0f38381a0b2b069ec3b74296ba
Aaron Spiteri <azzmosphere@gmail.com>
2025-01-15 21:51:37 +1100

    fixed MSP104 command, simplified CmakeLists.txt

--- src/fatcnt/events/msp104/msp104_hbridge_serializer.cpp
+++ src/fatcnt/events/msp104/msp104_hbridge_serializer.cpp
@@ -7,30 +7,17 @@ using namespace rrobot;
  */
 Event* Msp104Serializer::deserialize(std::vector<uint8_t> m) {
     // For land drone the only concern is yaw and throttle.
-    float yaw = _ratioD.scale(m[2]);
-    float throttle = _ratioD.scale(m[3]);
+    _yaw = _ratioD.scale(m[2]);
+    _throttle = _ratioD.scale(m[3]);
 
-    uint16_t motor1 = T(throttle), motor2 = T(throttle), motor3 = 0, motor4 = 0;
-    uint8_t in = 0;
-
-    if (throttle < 0) {
-        // Controller is moved right
-        if (yaw == 0) {
-            in = 0b0101;
-        } else if (yaw > 0) {
-            in = 0b0110;
-        } else {
-            in = 0b1001;
-        }
-    } else {
-        if (yaw == 0) {
-            in = 0b1010;
-        } else if (yaw > 0) {
-            in = 0b1001;
-        } else {
-            in = 0b0110;
-        }
+    uint16_t motor1 = T(_throttle), motor2 = T(_throttle), motor3 = 0, motor4 = 0;
+    uint8_t in = (_throttle >= 0) ? EN_FORWARD: EN_BACKWARD;
+    if (_yaw > 0) {
+        in = EN_RIGHT;
+    } else if (_yaw < 0) {
+        in = EN_LEFT;
     }
+
     msp_set_motor_hbridge *payload = new msp_set_motor_hbridge();
     payload->set_in(in);
     payload->set_motor1(motor1);
@@ -42,33 +29,16 @@ Event* Msp104Serializer::deserialize(std::vector<uint8_t> m) {
 }
 
 std::vector<uint8_t>  Msp104Serializer::serialize(Event* event) {
-    msp_set_motor_hbridge payload = event->getPayload<msp_set_motor_hbridge>();
-    float yaw = 0, throttle = 0;
-
-    if (payload.get_in() ==  0b0110) {
-            yaw = 0.5;
-            throttle = payload.get_motor1() / 1000;
-    } else if (payload.get_in() ==  0b1001) {
-        throttle = payload.get_motor1() / 1000;
-        yaw = -0.5;
-    } else if (payload.get_in() == 0b1010) {
-        yaw = -0.5;
-        throttle = (payload.get_motor1() / 1000) * -1;
-    } else if (payload.get_in() == 0b0110) {
-        yaw = 0.5;
-        throttle = (payload.get_motor1() / 1000) * -1;
-    }
     
-    std::vector<uint8_t> m;
-    m.clear();
-    m.reserve(8);
-    m.push_back(_ratioS.scale(0));
-    m.push_back(_ratioS.scale(0));
-    m.push_back(_ratioS.scale(yaw));
-    m.push_back(_ratioS.scale(throttle));
-    m.push_back(_ratioS.scale(0));
-    m.push_back(_ratioS.scale(0));
-    m.push_back(_ratioS.scale(0));
-    m.push_back(_ratioS.scale(0));
+    std::vector<uint8_t> m {
+        _ratioS.scale(0),
+        _ratioS.scale(0),
+        _ratioS.scale(_yaw),
+        _ratioS.scale(_throttle),
+        _ratioS.scale(0),
+        _ratioS.scale(0),
+        _ratioS.scale(0),
+        _ratioS.scale(0)
+    };
     return m;
 }
--- src/fatcnt/events/msp104/msp104_hbridge_serializer.hpp
+++ src/fatcnt/events/msp104/msp104_hbridge_serializer.hpp
@@ -29,6 +29,13 @@ namespace rrobot {
             Ratio<uint8_t, float> _ratioD = Ratio<uint8_t, float>( -1, 1, 0, 255);
             Ratio<float, uint8_t> _ratioS = Ratio<float, uint8_t>(0, 2455, -1, 1);
             uint16_t T(float i) {return static_cast<uint16_t>(((i<0)?i*-1:i)*1000);}
+            float _yaw;
+            float _throttle;
+
+            const uint8_t EN_BACKWARD =  0b0101;
+            const uint8_t EN_FORWARD =  0b1010;
+            const uint8_t EN_LEFT = 0b0110;
+            const uint8_t EN_RIGHT = 0b1001;
     };
 }
 
