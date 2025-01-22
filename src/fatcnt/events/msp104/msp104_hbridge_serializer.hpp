e55be8561f0790a82e491245943d855f51bc3781
Aaron Spiteri <azzmosphere@gmail.com>
2025-01-22 23:08:49 +1100

    Forward and backwards corrected.

--- src/fatcnt/events/msp104/msp104_hbridge_serializer.hpp
+++ src/fatcnt/events/msp104/msp104_hbridge_serializer.hpp
@@ -2,6 +2,7 @@
 #define MSP104_HBRIDGE_SERIALIZER
 
 #include <vector>
+#include <algorithm>
 #include <fatcnt/events/serializer.hpp>
 #include <fatcnt/math/ratio.hpp>
 #include <fatcnt/protocols/common/curators/rrp/msp_motor.hpp>
@@ -27,15 +28,15 @@ namespace rrobot {
 
         private:
             Ratio<uint8_t, uint16_t> _ratioTd = Ratio<uint8_t, uint16_t>(0, 255, 0, 1000);
-            Ratio<uint8_t, float> _ratioD = Ratio<uint8_t, float>(0, 255, -1, 1);
+            Ratio<uint8_t, int8_t> _ratioD = Ratio<uint8_t, int8_t>(0, 255, -1, 1);
 
 
             Ratio<uint16_t, uint8_t> _ratioTs = Ratio<uint16_t, uint8_t>(0, 1000, 0, 255);
-            Ratio<float, uint8_t> _ratioS = Ratio<float, uint8_t>(-1, 1, 0, 255);
+            Ratio<int8_t, uint8_t> _ratioS = Ratio<int8_t, uint8_t>(-1, 1, 0, 255);
 
-            uint8_t _yaw;
+            int8_t _yaw;
             uint16_t _throttle;
-            uint8_t _pitch;
+            int8_t _roll;
 
             const uint8_t EN_BACKWARD =  0b0101;
             const uint8_t EN_FORWARD =  0b1010;
--- test/test_msp104_hbridge_serializer.cpp
+++ test/test_msp104_hbridge_serializer.cpp
@@ -21,7 +21,7 @@ TEST_F(TestHbridgeSerializer, TestForward) {
     // yaw = -1 : 0
     // throttle 500 : 0
 
-    std::vector<uint8_t> m = {0,127,0,127,0,0,0,0};
+    std::vector<uint8_t> m = {255,0,128,127,0,0,0,0};
     Event* event = _hbridgeSerializer.deserialize(m);
 
     msp_set_motor_hbridge payload = event->getPayload<msp_set_motor_hbridge>();
@@ -31,4 +31,17 @@ TEST_F(TestHbridgeSerializer, TestForward) {
     EXPECT_EQ(498, payload.get_motor2());
     EXPECT_EQ(0, payload.get_motor3());
     EXPECT_EQ(0, payload.get_motor4());
+}
+
+TEST_F(TestHbridgeSerializer, TestBackward) {
+    std::vector<uint8_t> m = {0,0,128,127,0,0,0,0};
+    Event* event = _hbridgeSerializer.deserialize(m);
+
+    msp_set_motor_hbridge payload = event->getPayload<msp_set_motor_hbridge>();
+
+    EXPECT_EQ(0b0101, payload.get_in());
+    EXPECT_EQ(498, payload.get_motor1());
+    EXPECT_EQ(498, payload.get_motor2());
+    EXPECT_EQ(0, payload.get_motor3());
+    EXPECT_EQ(0, payload.get_motor4());   
 }
