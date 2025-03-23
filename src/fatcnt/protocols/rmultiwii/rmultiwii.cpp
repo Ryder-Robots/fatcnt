20c2aa23984afb74c97342e00a1f1498ce7eb1a5
Aaron Spiteri <azzmosphere@gmail.com>
2025-03-23 19:53:02 +1100

    started integrating multiwii and adding unit tests

--- src/fatcnt/protocols/rmultiwii/rmultiwii.cpp
+++ src/fatcnt/protocols/rmultiwii/rmultiwii.cpp
@@ -2,11 +2,39 @@
 
 using namespace rrobot;
 
-RmMultiWii RmMultiWii::createInstance(uint8_t* data, Crc32 crc) {
-    return RmMultiWii(MSPCOMMANDS::MSP_SENSOR, 0);
+dlib::logger dlog_mw("rr_curator");
+
+RmMultiWii RmMultiWii::createInstance(uint8_t* data, Crc32 crc, Environment* environment) {
+    dlog_mw.set_level(environment->getLogging().getLogLevel());
+    try {
+        MSPCOMMANDS cmd = getSupportCommand(data[0]);
+        int16_t sz = static_cast<uint16_t>(data[1] | (data[2] << 8));
+        int32_t crc32 = static_cast<uint32_t>(data[3] | data[4] << 8 | data[5] << 16 | data[6] << 24);
+
+        uint8_t* payload = new uint8_t[sz];
+        for (int i = 0; i < sz; i++) {
+            payload[i] = data[i + 7];
+
+            if (data[i + 7] == _TERMINATION_CHAR) {
+                throw RrIOException("unexpected character in sequence");
+            }
+        }
+
+        RmMultiWii result = RmMultiWii(cmd, sz);
+        if (sz > 0) {
+            if (crc32 != crc.calculate(payload, sz)) {
+                throw RrIOException("failed CRC check");
+            }
+            result.setPayload(payload);
+        }
+        return result;
+    } catch (const std::out_of_range& e) {
+        dlog_mw << dlib::LERROR << "could not create instance - out of bounds error recieved : " << e.what();
+        return RmMultiWii(MSPCOMMANDS::MSP_ERROR, 0);
+    } catch (const RrIOException& e) {
+        dlog_mw << dlib::LERROR << "could not create instance - unexpected input recieved : " << e.what();
+        return RmMultiWii(MSPCOMMANDS::MSP_ERROR, 0);
+    }
 }
 
-
-uint8_t* RmMultiWii::encode(Crc32 crc) {
-    return nullptr;
-}
+uint8_t* RmMultiWii::encode(Crc32 crc) { return nullptr; }
---
+++ test/test_rmultiwii.cpp
@@ -0,0 +1,48 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
+#include <fatcnt/protocols/rmultiwii/rmultiwii.hpp>
+#include <fatcnt/environment/environmentProcessor.hpp>
+
+using namespace rrobot;
+using json = nlohmann::json;
+
+namespace fs = std::filesystem;
+
+class TestRatio : public ::testing::Test {
+    protected:
+     void SetUp() override {
+         // Setup code
+     }
+ 
+     void TearDown() override {
+         // Teardown code
+     }
+ };
+
+ TEST_F(TestRatio, shouldEncodeStatus) {
+    const fs::path filepath = "manifests/virtual.json";
+    ifstream ifs(filepath);
+    json manifest = json::parse(ifs);
+    ifs.close();
+    Environment* environment = EnviromentProcessor::createEnvironmentRef(manifest);
+
+
+    Crc32 crc = Crc32();
+    uint8_t data[] = {101, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1E};
+    RmMultiWii multiWii = RmMultiWii::createInstance(data, crc, environment);
+
+    ASSERT_EQ(MSPCOMMANDS::MSP_STATUS, multiWii.getCommand());
+    ASSERT_EQ(0, multiWii.getSize());
+    
+    // uint8_t* data2 = multiWii.encode(crc);
+
+    // for (int i = 0; i < 8; i++) {
+    //     ASSERT_EQ(data[i], data2[i]);
+    // }
+ }
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
